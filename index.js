0865ad38ac6b2b0f0710baae3a34d0fd9f10b219
Vladimir Sobolev <v.sobolev@gmail.com>
2025-05-11 19:56:57 +0300

    fix

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 'use strict';
 
 const AccessoryImpl = require('./accessory');
-const PlatformImpl = require('./platform');
 
 let Service, Characteristic;
 
 module.exports = function (homebridge) {
-  Service = homebridge.hap.Service;
-  Characteristic = homebridge.hap.Characteristic;
+	Service = homebridge.hap.Service;
+	Characteristic = homebridge.hap.Characteristic;
 
-  homebridge.registerAccessory('homebridge-http-garage-doors', 'HttpGarageDoors', AccessoryImpl);
-  homebridge.registerPlatform('homebridge-http-garage-doors', 'HttpGarageDoors', PlatformImpl);
+	homebridge.registerAccessory('homebridge-http-garage-doors', 'HttpGarageDoors', AccessoryImpl);
 };

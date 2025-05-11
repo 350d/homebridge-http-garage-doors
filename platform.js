3fef7d279223b2d9f9b1071a7db71a53212479b7
Vladimir Sobolev <v.sobolev@gmail.com>
2025-05-11 19:27:39 +0300

    update

--- platform.js
+++ platform.js
@@ -1,48 +1,43 @@
-'use strict';
+module.exports = (homebridge) => {
+	return class HttpGarageDoorsPlatform {
+		constructor(log, config, api) {
+			this.log = log;
+			this.config = config;
+			this.api = api;
+			this.accessories = [];
 
-const { v4: uuidv4 } = require('uuid');
+			const { Service, Characteristic } = api.hap;
+			this.Service = Service;
+			this.Characteristic = Characteristic;
 
-let Service, Characteristic;
+			api.on('didFinishLaunching', () => {
+				this.log.debug('Executed didFinishLaunching callback');
+				this.discoverDevices();
+			});
+		}
 
-class HttpGarageDoorsPlatform {
-  constructor(log, config, api) {
-    this.log = log;
-    this.config = config;
-    this.api = api;
-    this.accessories = [];
+		configureAccessory(accessory) {
+			this.log.info('Loading accessory from cache:', accessory.displayName);
+			this.accessories.push(accessory);
+		}
 
-    Service = this.api.hap.Service;
-    Characteristic = this.api.hap.Characteristic;
+		discoverDevices() {
+			const name = this.config.name || 'Garage Door';
+			const uuid = this.api.hap.uuid.generate(name);
+			const existingAccessory = this.accessories.find(a => a.UUID === uuid);
 
-    this.api.on('didFinishLaunching', () => {
-      this.log.debug('Executed didFinishLaunching callback');
-      this.discoverDevices();
-    });
-  }
+			if (existingAccessory) {
+				this.log.info('Restoring accessory from cache:', existingAccessory.displayName);
+			} else {
+				const accessory = new this.api.platformAccessory(name, uuid);
+				accessory.getService(this.Service.AccessoryInformation)
+					.setCharacteristic(this.Characteristic.Manufacturer, 'METATAG')
+					.setCharacteristic(this.Characteristic.Model, 'HTTP_GARAGE_GATES')
+					.setCharacteristic(this.Characteristic.SerialNumber, '00000001');
 
-  configureAccessory(accessory) {
-    this.log.info('Loading accessory from cache:', accessory.displayName);
-    this.accessories.push(accessory);
-  }
-
-  discoverDevices() {
-    const name = this.config.name || 'Garage Door';
-    const uuid = this.api.hap.uuid.generate(name);
-    const existingAccessory = this.accessories.find(a => a.UUID === uuid);
-
-    if (existingAccessory) {
-      this.log.info('Restoring accessory from cache:', existingAccessory.displayName);
-    } else {
-      const accessory = new this.api.platformAccessory(name, uuid);
-      accessory.getService(Service.AccessoryInformation)
-        .setCharacteristic(Characteristic.Manufacturer, 'METATAG')
-        .setCharacteristic(Characteristic.Model, 'HTTP_GARAGE_GATES')
-        .setCharacteristic(Characteristic.SerialNumber, '00000001');
-
-      accessory.addService(Service.GarageDoorOpener);
-      this.api.registerPlatformAccessories('homebridge-http-garage-doors', 'HttpGarageDoors', [accessory]);
-    }
-  }
-}
-
-module.exports = HttpGarageDoorsPlatform;
+				accessory.addService(this.Service.GarageDoorOpener);
+				this.api.registerPlatformAccessories('homebridge-http-garage-doors', 'HttpGarageDoors', [accessory]);
+			}
+		}
+	};
+};
